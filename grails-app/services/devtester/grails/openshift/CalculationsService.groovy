8da9e0e55abde42d1d479bfbc72708c74e77e9db
iulian.petcu <levelik@yahoo.com>
2019-04-23 18:02:07 +0300

    update project before GORM

---
+++ grails-app/services/devtester/grails/openshift/CalculationsService.groovy
@@ -0,0 +1,31 @@
+package devtester.grails.openshift
+
+import grails.gorm.transactions.Transactional
+
+@Transactional
+class CalculationsService {
+
+    def welcome(params) {
+
+        def firstName = params?.firstName
+        def totalPoints = params.points?.toInteger()
+
+        def welcomeMessage = ""
+
+        switch (totalPoints) {
+            case 5:
+                welcomeMessage = "Welcome back $firstName, this drink is on us"
+                break
+            case 4:
+                welcomeMessage = "Welcome back $firstName, your next drink is free"
+                break
+            case 2..3:
+                welcomeMessage = "Welcome back $firstName, you have $totalPoints points"
+                break
+            default:
+                welcomeMessage = "Welcome $firstName. Thanks for registering"
+        }
+
+    }
+
+}
